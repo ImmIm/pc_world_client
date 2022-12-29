c416f1dd90a266d39f6064034f36391f02795278
Kiryl Savich <48187113+ImmIm@users.noreply.github.com>
2022-12-29 20:48:24 +0200

    commit recovery

--- src/components/Products/ProductsList.tsx
+++ src/components/Products/ProductsList.tsx
@@ -1,8 +1,17 @@
+import { Container } from '@mui/material'
+import ryzen from '../../assets/Ryzen5.jpg'
 import React from 'react'
+import ProductCard from './ProductCard'
 
+
+const products = [{id: 1, name: 'Ryzen 5', image:ryzen, frequency: '3200 Mhz', cores: 6 }]
 function ProductsList() {
   return (
-    <div>ProductsList</div>
+    <Container >
+      {products.map((el) => {
+       return <ProductCard />
+      })}
+    </Container>
   )
 }
 
